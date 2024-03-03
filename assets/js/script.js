4774884e7593a9c3a68c33b1e63b7c2da1892c15
ilya.sinitsa <ilya.sinitsa@yandex.ru>
2024-03-03 16:06:41 +0300

    Finish desktop version

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $( '.header__menu-btn' ).click(function() {
         $(this).toggleClass('active');
     });
+
+    const swiper = new Swiper('.examples__slider', {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.examples__slider-control_next',
+          prevEl: '.examples__slider-control_prev',
+        }
+    });
 });
